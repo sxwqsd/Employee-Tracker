bf626a599c16dd6db7c189b1062e027769423134
squid <mercado.valeria856@gmail.com>
2021-09-19 23:53:19 -0700

    finish

--- index.js
+++ index.js
@@ -1,175 +1,212 @@
-const inquirer = require("inquirer")
-const mysql = require("mysql")
+const inquirer = require('inquirer')
+const mysql = require('mysql2')
 const cTable = require('console.table');
 
+
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3000,
-    user: "root",
-    password: "password",
-
-    database: "employee_trackerDB"
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'sawqPlop143$',
+    database: 'employee_trackerdb'
 });
 
-connection.connect(function() {
-    console.log("connected as id" + connection.threadId);
-    startApp();
-});
+// connection.query(
+//     'SELECT * FROM department',
+//     function(err, res) {
+//         console.log('Connecting...')
+//         if (err) throw err;
+//         console.log(res)
+//     }
+// )
 
 const departQuest = [
     {
-        name: "name",
-        type: "input",
-        message: "Enter new department name."
+        name: 'name',
+        type: 'input',
+        message: 'Enter new department name.'
     }
 ];
 
 const employeeQuest = [
     {
-        name: "firstName",
-        input: "input",
-        message: "Enter employee's first name.",
+        name: 'firstName',
+        input: 'input',
+        message: "Enter employees first name.",
     },
     {
-        name: "lastName",
-        type: "input",
-        message: "Enter employee's last name.",
+        name: 'lastName',
+        type: 'input',
+        message: "Enter employees last name.",
     },
     {
-        name: "role",
-        type: "input",
+        name: 'role',
+        type: 'input',
         message: "Enter employee's role.",
     },
     {
-        name: "manager",
-        type: "input",
+        name: 'manager',
+        type: 'input',
         message: "Enter employee's manager.",
     }
 ];
 
-const roleQuest = [
+const ROLQuest = [
     {
-        name: "name",
-        type: "input",
-        message: "Enter role name."
+        name: 'name',
+        type: 'input',
+        message: 'Enter role name.'
     },
     {
-        name: "salary",
-        type: "input",
-        message: "Enter role salary"
+        name: 'salary',
+        type: 'input',
+        message: 'Enter role salary'
 
     }
 ];
 
-const menue = [
+const menu = [
     {
-        name: "menue",
-        type: "list",
-        message: "MENUE",
+        name: 'menu',
+        type: 'list',
+        message: 'MENU',
         choices: [
-            "View all departments",
-            "View all roles",
-            "View all employees",
-            "Add a department",
-            "Add a role",
-            "Add an employee",
-            "Update an employee"
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee'
         ]
     }
 ];
 
-
 function startApp() {
-    inquirer.prompt(menue)
+    inquirer.prompt(menu)
         .then(function (val) {
-            switch (val.choice) {
-                case "View all departments":
+            switch (val.menu) {
+                case 'View all departments':
                     viewDepart();
                     break;
 
-                case "View all roles":
+                case 'View all roles':
                     viewRole();
                     break;
 
-                case "View all employees":
+                case 'View all employees':
                     viewEmployee();
                     break;
 
-                case "Add a department":
+                case 'Add a department':
                     addDepart();
                     break;
 
-                case "Add a role":
-                    addRole();
+                case 'Add a role':
+                    addROL();
                     break;
 
-                case "Add an employee":
+                case 'Add an employee':
                     addEmployee();
                     break;
 
-                case "Update an employee":
+                case 'Update an employee':
                     updateEmployee();
                     break;
-
+                default:
+                    connection.end()
+                    break;
             }
+        })
+        .catch(function (err) {
+            console.log(err)
         })
 }
 
+function viewDepart() {
+    connection.query(`SELECT * FROM department`, function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        startApp();
+        
+    })
+}
+
+function viewRole() {
+    connection.query(`SELECT * FROM ROL`, function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        startApp();
+    })
+}
 function addDepart() {
     inquirer.prompt(departQuest)
         .then(function (res) {
             var query = connection.query(
-                "INSERT INTO department SET ?",
+                `INSERT INTO department SET ?`,
                 {
                     name: res.name
-                },
-                function () {
-                    console.table(res);
-                    startPrompt();
-                }
-
-            )
-        }
+                 } ,
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                startApp();
+            }
         )
-};
-
-function addRole() {
-    connection.query("SELECT role.name AS name, role.salary AS salary FROM role", function (res) {
-        inquirer.prompt(roleQuest)
-            .then(function (res) {
-                connection.query(
-                    "INSERT INTO role SET ?",
-                    {
-                        name: res.name,
-                        salary: res.salary,
-                    },
-                    function () {
-                        
-                        console.table(res);
-                        startPrompt();
-                    })
-
-            });
-    });
-};
-
-function addEmployee(){
-    inquirer.prompt(employeeQuest)
-    .then(function(val){
-        var roleId = selectRole().indexOf(val.role) + 1
-      var managerId = selectManager().indexOf(val.choice) + 1
-      connection.query("INSERT INTO employee SET ?", 
-      {
-          first_name: val.firstName,
-          last_name: val.lastName,
-          manager_id: managerId,
-          role_id: roleId
-          
-      }, function(){
-          console.table(val)
-          startPrompt()
-      })
-    
     })
+  };
+
+function addROL() {
+    connection.query('SELECT ROL.title AS Title, ROL.salary AS Salary FROM ROL', function (err, res) {
+        inquirer.prompt(ROLQuest)
+        .then(function (res) {
+            connection.query(
+                'INSERT INTO ROL SET ?',
+                {
+                    title: res.Title,
+                    salary: res.Salary,
+                },
+                function (err) {
+                    if (err) throw err
+                    console.table(res);
+                    startApp();
+                }
+            )
+
+        });
+    });
+}
+
+
+function addEmployee() {
+    inquirer.prompt(employeeQuest)
+        .then(function (val) {
+            var ROL_ID = selectRole().indexOf(val.ROL) + 1
+            var managerId = selectManager().indexOf(val.choice) + 1
+            connection.query(`INSERT INTO employee SET ?`,
+                {
+                    first_name: val.firstName,
+                    last_name: val.lastName,
+                    manager_id: managerId,
+                    role_id: ROL_ID
+
+                }, function (err) {
+                    if (err) throw err
+                    console.table(val)
+                    startApp()
+                })
+
+        })
 };
+
+function viewEmployee() {
+    connection.query("SELECT employee.first_name, employee.last_name, ROL.title, ROL.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN ROL on ROL.id = employee.ROL_id INNER JOIN department on department.id = ROL.department_id left join employee e on employee.manager_id = e.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      startApp()
+  })
+}
+
+startApp()
 
--- schema.sql
+++ schema.sql
@@ -1,26 +1,30 @@
-DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE DATABASE employee_trackerDB;
-USE employee_trackerDB;
+DROP DATABASE IF EXISTS employee_trackerdb;
+CREATE DATABASE employee_trackerdb;
+USE employee_trackerdb;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
 );
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  manager_id INT,
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
-);
-CREATE TABLE role (
+
+CREATE TABLE ROL
+ (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  ROL_id INT,
+  FOREIGN KEY (ROL_id) 
+  REFERENCES ROL(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 /*Department*/
@@ -32,39 +36,52 @@ INSERT INTO department (name)
 VALUE ("Managment");
 
 
-/*Roles*/
-INSERT INTO role (title, salary, department_id) 
+/*ROLs*/
+INSERT INTO ROL
+ (title, salary, department_id) 
 VALUE ("Sr. Engineer", 80000, 1);
-INSERT INTO role (title, salary, department_id) 
+INSERT INTO ROL
+ (title, salary, department_id) 
 VALUE ("Jr. Engineer", 60000, 1);
-INSERT INTO role (title, salary, department_id) 
+INSERT INTO ROL
+ (title, salary, department_id) 
 VALUE ("Architect", 70000, 1);
-INSERT INTO role (title, salary, department_id) 
+INSERT INTO ROL
+ (title, salary, department_id) 
 VALUE ("Math Guy", 75000, 2);
-INSERT INTO role (title, salary, department_id) 
-VALUE ("Other Math GUy", 65000, 2);
-INSERT INTO role (title, salary, department_id) 
+INSERT INTO ROL
+ (title, salary, department_id) 
+VALUE ("Other Math Guy", 65000, 2);
+INSERT INTO ROL
+ (title, salary, department_id) 
 VALUE ("Regional Manager", 120000, 3);
-INSERT INTO role (title, salary, department_id) 
+INSERT INTO ROL
+ (title, salary, department_id) 
 VALUE ("Assistant to the Regional Manager", 100000, 3);
 
 
 /*Employees*/
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, ROL
+_id)
 VALUE ("James", "Bond", null, 1);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, ROL
+_id)
 VALUE ("Jim", "Halpert", null, 2);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, ROL
+_id)
 VALUE ("Pam", "Halpert", null, 3);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, ROL
+_id)
 VALUE ("Dwight", "Schrute", 6, 4);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, ROL
+_id)
 VALUE ("David", "Mackelmore", 6, 5);
-INSERT INTO employee (first_name, last_name, manager_id, role_id)
+INSERT INTO employee (first_name, last_name, manager_id, ROL
+_id)
 VALUE ("Beep", "Boop", 6, 6);
 
 SELECT * FROM department;
-SELECT * FROM role;
+SELECT * FROM ROL;
 SELECT * FROM employee;
 
 
